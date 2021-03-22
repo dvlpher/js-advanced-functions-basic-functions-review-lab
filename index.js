6f6010feeb9e2f3c0d0d4edf5ec3f5c5a145381b
CelesteSMThomas <celestesmthomas@gmail.com>
2021-03-21 18:13:21 -0700

    commit tree b6bf6e57bf7271b311a4ebd5d1ce323919483222

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function wrapAdjective(msg = "*") {
+  return function (string = "special") {
+    return `You are ${msg}${string}${msg}!`;
+  };
+}
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+const Calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+};
+
+function actionApplyer(integer, arr) {
+  if (arr.length === 0) return 0;
+  else {
+    return (integer * 2 + 1000) % 7;
+  }
+}
+
+// let actionApplyer = function (start, ray) {
+//   let a = start;
+
+//   for (let i = 0; i < ray.length; i++) {
+//     a = ray[i](a);
+//   }
+
+//   return a;
+// };
